fafdc6646654b2acae0df557b33b9814341d8eab
axellfumioo <axelazharputra@gmail.com>
2025-06-28 21:54:50 +0700

    Add deployment workflow for VPS using GitHub Actions

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello from CI/CD Express API!');
 });
+app.get('/axelganteng', (req, res) => {
+  res.send('Axel adalah orang terganteng');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
